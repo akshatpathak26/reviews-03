fd3e98952eba43fc8ad474891100c95dd27693b2
Akshat <akshatpathak08@gmail.com>
2022-04-14 00:15:41 +0530

    reviews

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Review from './Review';
+
+function App() {
+  return (
+    <main>
+      <section className='container'>
+        <div className="title">
+          <Review/>
+        </div>
+      </section>
+    </main>
+  )
+}
+
+export default App;
